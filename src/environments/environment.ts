feca526dada042cdac1945ac39c8a7d21cf046b1
Rodrigo Kammer <rodrigo.kammer@gmail.com>
2018-05-28 13:16:09 -0700

    change

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+    production: false,
+    firebase: {
+        apiKey              : 'AIzaSyDrn5GYT5EqZKayvS1Oli-EifZb235q2hM',
+        authDomain          : 'kitchen-214.firebaseapp.com',
+        databaseURL         : 'https://kitchen-214.firebaseio.com',
+        projectId           : 'kitchen-214',
+        storageBucket       : '',
+        messagingSenderId   : '899688327088'
+    }
 };
 
 /*
